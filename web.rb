c199cb3e22d825cee0b8d1c448a7b7a4506fe1f4
Nigel Thorne <github@nigelthorne.com>
2013-11-21 18:19:46 +1100

    first version

---
+++ web.rb
@@ -0,0 +1,50 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'haml'
+
+# Goal
+## suggest ideas
+## vote ideas
+## toggle vote per idea per person
+## remove ideas with no votes
+
+
+@@votes ||= {}
+
+enable :sessions
+
+get '/' do
+	@user = session[:user]
+	@votes = @@votes
+	haml :index
+end
+
+post '/add_idea' do
+  voters = @@votes[params["idea"]] ||= []
+  voters << session[:user]
+  voters.uniq!
+  redirect '/'
+end
+
+post '/vote' do
+  voters = @@votes[params["idea"]]
+  name = session[:user]
+  if voters.include? name
+	voters.delete name
+	@@votes.delete(params["idea"]) if voters.empty?
+  else
+	voters << name
+  end
+  redirect '/'
+end
+
+post '/login' do
+  session[:user] = params["name"]
+  redirect '/'
+end
+
+post '/logout' do
+  session[:user] = nil
+  redirect '/'
+end
+
